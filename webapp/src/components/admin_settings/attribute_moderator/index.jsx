283226fba57cc6d4e1967a134d3a8f6356c78b57
rfyuz <eskil.nyberg@gmail.com>
2025-01-04 19:50:26 +0100

    Trial and terror #2

--- webapp/src/components/admin_settings/attribute_moderator/index.jsx
+++ webapp/src/components/admin_settings/attribute_moderator/index.jsx
@@ -1,6 +1,6 @@
 import {connect} from 'react-redux';
 
-import mapDispatchToProps from '../abstract_attribute/index.jsx';
+import mapDispatchToProps from '../abstract_attribute';
 import AttributeModerator from './attribute_moderator';
 
 export default connect(null, mapDispatchToProps)(AttributeModerator);
