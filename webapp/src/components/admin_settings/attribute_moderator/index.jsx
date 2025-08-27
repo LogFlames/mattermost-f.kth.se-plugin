553248b7b79d8e41e0fe690f01d9c193ffe3cf31
Elias Lundell <elilun03@gmail.com>
2025-08-27 14:40:15 +0200

    add getteams to moderator attributes

--- webapp/src/components/admin_settings/attribute_moderator/index.jsx
+++ webapp/src/components/admin_settings/attribute_moderator/index.jsx
@@ -3,7 +3,7 @@ import {bindActionCreators} from 'redux';
 import {getCustomEmojisInText} from 'mattermost-redux/actions/emojis';
 
 import {getProfilesByIds} from 'mattermost-redux/actions/users';
-import {getTeam} from 'mattermost-redux/actions/teams';
+import {getTeam, getTeams} from 'mattermost-redux/actions/teams';
 import {getChannel} from 'mattermost-redux/actions/channels';
 
 import AttributeModerator from './attribute_moderator.jsx';
@@ -13,6 +13,7 @@ function mapDispatchToProps(dispatch) {
         actions: bindActionCreators({
             getProfilesByIds,
             getTeam,
+            getTeams,
             getChannel,
             getCustomEmojisInText,
         }, dispatch),
