bd8473879db3befc222c71af0ed461de53035bca
Elias Lundell <elilun03@gmail.com>
2025-08-27 14:17:38 +0200

    increase upload limit

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 old_size=$(echo "/opt/mattermost/bin/mmctl config get FileSettings.MaxFileSize --local" | ssh fmm "/bin/bash")
 echo "Saved original value of max file upload: $old_size"
 
-echo "/opt/mattermost/bin/mmctl config set FileSettings.MaxFileSize 104857600 --local" | ssh fmm "/bin/bash"
+echo "/opt/mattermost/bin/mmctl config set FileSettings.MaxFileSize 268435456 --local" | ssh fmm "/bin/bash"
 
 source ./set_secret.sh
 MM_SERVICESETTINGS_SITEURL="https://mattermost.fysiksektionen.se" make deploy
