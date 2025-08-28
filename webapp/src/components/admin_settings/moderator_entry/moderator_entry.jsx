60332bf33b831e1af1046e0f585772d3c049b505
Elias Lundell <elilun03@gmail.com>
2025-08-28 14:55:56 +0200

    fix import

--- webapp/src/components/admin_settings/moderator_entry/moderator_entry.jsx
+++ webapp/src/components/admin_settings/moderator_entry/moderator_entry.jsx
@@ -1,4 +1,4 @@
-import AbstractEntry from '../abstract_entry';
+import AbstractEntry from '../abstract_entry/abstract_entry.jsx';
 import ChannelsInput from '../inputs/channels_input';
 
 export default class ModeratorEntry extends AbstractEntry {
